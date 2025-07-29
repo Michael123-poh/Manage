ddcb0d3fb45fb77c661e43e4b976a86f18031d7f
Michael123-poh <99747838+Michael123-poh@users.noreply.github.com>
2025-07-30 00:05:29 +1000

    Update Y_acheteurDetailController.php
    Ajout des paiement et verification de la somme final

--- Y_acheteurDetailController.php
+++ Y_acheteurDetailController.php
@@ -20,13 +20,47 @@ $dateNaisssance = date('d/m/Y', strtotime($Y_executeAcheteurDetail->dateNaisAche
 $Y_executeHistoriqueTransaction = F_executeRequeteSql('SELECT * FROM transactions INNER JOIN versements ON transactions.idVersement = versements.idVersement WHERE idAcheteur = ?', [$Y_idAcheteur]);
 
 // 4. Afficher le montant Total a payer, le montant verse, le montant restant
-$Y_executeMontantTotal = F_executeRequeteSql('SELECT montantTotalSelection, montantVersement FROM selection INNER JOIN versements ON selection.idSelection = versements.idSelection WHERE versements.idAcheteur = ?', [$Y_idAcheteur]);
+$Y_executeMontantTotal = F_executeRequeteSql('SELECT montantTotalSelection, montantVersement, idVersement FROM selection INNER JOIN versements ON selection.idSelection = versements.idSelection WHERE versements.idAcheteur = ?', [$Y_idAcheteur]);
+
+$idVersement = $Y_executeMontantTotal->idVersement ?? null;
+$Employe = 'EMP00001';
 
 // 5. Calcule du pourcentage de paiement
 $total = $Y_executeMontantTotal->montantTotalSelection ?? 0;
 $paye = $Y_executeMontantTotal->montantVersement ?? 0;
 $pourcentage = $total > 0 ? round(($paye / $total) * 100) : 0;
 
+// 6. Enregistrer un nouveau paiement
+if (isset($_POST['Enregistrer'])){
+    extract($_POST);
+
+    // Verifier si le montant Total est egale au montant verse
+    if ($paye < $total) {
+        // Reecuperer le dernier ID de transaction
+        $Y_executeGetLastIdTransaction = F_executeRequeteSql('SELECT MAX(idTransaction) AS lastId FROM transactions');
+
+        foreach ($Y_executeGetLastIdTransaction as $lastId) {
+            $dernierIdTransaction = $lastId->lastId;
+        }
+        
+        // Generer un nouvel ID de transaction
+        $idTransaction = F_genereMatricule($dernierIdTransaction, 'TRS00001');
+
+        // Insertion du paiement dans la base de données
+        $Y_executeInsertPaiement = F_executeRequeteSql('INSERT INTO transactions (idTransaction, montantTransaction, modePaiement, dateTransaction, dateCreateTransaction, idVersement, idEmploye) VALUES (?, ?, ?, ?, NOW(), ?, ?)', 
+        [$idTransaction, $montant, $modePaiement, $dateVersement, $idVersement, $Employe]);
+
+        // Mettre à jour le montant du versement
+        $Y_executeUpdateVersement = F_executeRequeteSql('UPDATE versements SET montantVersement = montantVersement + ? WHERE idVersement = ?', 
+        [$montant, $idVersement]);
+
+        // Redirection vers la page de détails de l'acheteur
+        header("Location: Y_acheteurDetailController.php?Y_idAcheteur=$Y_idAcheteur");
+        exit;
+    } else {
+        $errorMessage = "Somme Total Atteint";
+    }
+}
 
 require('../views/acheteur/acheteurDetails.php');
 ?>
